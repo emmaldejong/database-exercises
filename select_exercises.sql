7b2a4e6a2959edabb46d220ee8ee7cdf30d8c829
emmadejong <dejong.l.emma@gmail.com>
2018-03-16 16:27:51 -0500

    added album seeder and select queries

---
+++ select_exercises.sql
@@ -0,0 +1,19 @@
+USE codeup_test_db;
+
+SELECT 'The name of all albums by Pink Floyd' AS 'Info';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'The year Sgt. Peppers Lonely Hearts Club Band was released' AS 'Info';
+SELECT release_date FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club';
+
+SELECT 'The genre of Nevermind' AS 'Info';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT 'Which albums were released in the 1990s' AS 'Info';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT 'Which albums had less than 20 million certified sales' AS 'Info';
+SELECT name FROM albums WHERE sales < 20;
+
+SELECT 'All the albums with a genre of "Rock"' AS 'Info';
+SELECT name FROM albums WHERE genre = 'Rock';
