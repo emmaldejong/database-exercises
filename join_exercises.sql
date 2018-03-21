0227394e4df67d423f6d65cfec40f0be10dc38ad
emmadejong <dejong.l.emma@gmail.com>
2018-03-21 15:37:06 -0500

    added exercise instructions to look back on

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,6 +1,17 @@
 USE employees;
 
 # Write a query that shows each department along with the name of the current manager for that department
+-- Department Name    | Department Manager
+-- --------------------+--------------------
+-- Customer Service   | Yuchang Weedman
+-- Development        | Leon DasSarma
+-- Finance            | Isamu Legleitner
+-- Human Resources    | Karsten Sigstam
+-- Marketing          | Vishwani Minakawa
+-- Production         | Oscar Ghazalie
+-- Quality Management | Dung Pesch
+-- Research           | Hilary Kambil
+-- Sales              | Hauke Zhang
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS Dept_Manager, d.dept_name AS Dept_Name
 FROM employees AS e
   JOIN dept_manager AS dm
@@ -11,6 +22,12 @@ WHERE dm.to_date >= curdate()
 ORDER BY d.dept_name;
 
 # Find the name of all departments currently managed by women
+-- Department Name | Manager Name
+-- ----------------+-----------------
+-- Development     | Leon DasSarma
+-- Finance         | Isamu Legleitner
+-- Human Resources | Karsetn Sigstam
+-- Research        | Hilary Kambil
 SELECT concat(e.first_name, ' ', e.last_name) AS Dept_Manager, d.dept_name AS Dept_Name
 FROM employees AS e
   JOIN dept_manager AS dm
@@ -22,7 +39,15 @@ ORDER BY d.dept_name;
 
 # Find the current titles of employees currently working in the Customer Service department
 -- titles to pull employees --> employees to pull departments
-
+-- Title              | Count
+-- -------------------+------
+-- Assistant Engineer |    68
+-- Engineer           |   627
+-- Manager            |     1
+-- Senior Engineer    |  1790
+-- Senior Staff       | 11268
+-- Staff              |  3574
+-- Technique Leader   |   241
 SELECT t.title AS Emp_Title, count(e.emp_no) AS Emp_Count
 FROM titles AS t
   JOIN employees AS e
@@ -36,6 +61,17 @@ GROUP BY Emp_Title;
 
 # Find the current salary of all current managers
 -- salaries to pull employees --> employee to pull dept_emp --> dept_emp to pull departments
+-- Department Name    | Name              | Salary
+-- -------------------+-------------------+-------
+-- Customer Service   | Yuchang Weedman   |  58745
+-- Development        | Leon DasSarma     |  74510
+-- Finance            | Isamu Legleitner  |  83457
+-- Human Resources    | Karsten Sigstam   |  65400
+-- Marketing          | Vishwani Minakawa | 106491
+-- Production         | Oscar Ghazalie    |  56654
+-- Quality Management | Dung Pesch        |  72876
+-- Research           | Hilary Kambil     |  79393
+-- Sales              | Hauke Zhang       | 101987
 SELECT dept.dept_name AS Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Manager_Name, s.salary AS Current_Salary
 FROM employees AS e
   JOIN dept_manager AS dm on dm.emp_no = e.emp_no
@@ -47,7 +83,14 @@ ORDER BY Department_Name;
 
 ## Find the names of all current employees, their department name, and their current manager's name
 -- employee --> dept_emp >>> dept_emp --> departments >>> departments --> dept_manager >>> dept_manager --> employee(manager)
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee_Name, d.dept_name AS Department_Name, CONCAT(e2.first_name, ' ', e2.last_name)
+-- 240,124 Rows
+--
+-- Employee Name | Department Name  |  Manager Name
+-- --------------|------------------|-----------------
+-- Huan Lortz   | Customer Service | Yuchang Weedman
+--
+-- .....
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee_Name, d.dept_name AS Department_Name, CONCAT(e2.first_name, ' ', e2.last_name) AS Manager_Name
 FROM employees AS e
   JOIN dept_emp AS emp
     ON e.emp_no = emp.emp_no
