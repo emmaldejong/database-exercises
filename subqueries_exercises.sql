ede8b3e658c8ee41d60e8239dc4c7f9cb61fd396
emmadejong <dejong.l.emma@gmail.com>
2018-03-21 16:51:22 -0500

    completed exercises and bonuses

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,9 +1,25 @@
 USE employees;
 
 # 1. Find all the employees with the same hire date as employee 101010 using a sub-query. (69 Rows)
+SELECT hire_date FROM employees WHERE emp_no = 101010;
+
+SELECT e.first_name, e.last_name, e.hire_date
+FROM employees AS e
+WHERE hire_date = (
+  SELECT hire_date
+  FROM employees
+  WHERE emp_no = 101010
+);
 
 # 2. Find all the titles held by all employees with the first name Aamod. (314 total titles, 6 unique titles)
-
+SELECT title
+FROM titles as t
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM employees
+  WHERE first_name = 'Aamod'
+)
+GROUP BY title;
 # 3. Find all the current department managers that are female.
 -- +------------+------------+
 -- | first_name | last_name  |
@@ -13,6 +29,13 @@ USE employees;
 -- | Leon       | DasSarma   |
 -- | Hilary     | Kambil     |
 -- +------------+------------+
+SELECT e.first_name, e.last_name
+FROM employees AS e
+WHERE e.gender = 'F' AND emp_no IN (
+  SELECT emp_no
+  FROM dept_manager
+  WHERE to_date >= CURDATE()
+);
 
 # Bonus
 # 1. Find all the department names that currently have female managers.
@@ -24,6 +47,17 @@ USE employees;
 -- | Human Resources |
 -- | Research        |
 -- +-----------------+
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+  SELECT dept_no
+  FROM dept_manager
+  WHERE to_date >= CURDATE() AND emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE gender = 'F'
+  )
+);
 
 # 2. Find the first and last name of the employee with the highest salary.
 -- +------------+-----------+
@@ -31,3 +65,13 @@ USE employees;
 -- +------------+-----------+
 -- | Tokuyasu   | Pesch     |
 -- +------------+-----------+
+SELECT first_name, last_name
+FROM employees as e
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM salaries
+  WHERE salary = (
+    SELECT MAX(salary)
+    FROM salaries
+  )
+);
