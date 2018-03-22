bfd007779af873f1bf0345812e7c2bb1a6d72436
emmadejong <dejong.l.emma@gmail.com>
2018-03-22 09:41:52 -0500

    add more bonus problems to practice

---
+++ relationships_bonuses.sql
@@ -0,0 +1,11 @@
+USE employees;
+
+-- ================================= RELATIONSHIP DRILLS
+-- concat the max salaries of the first two employeess
+SELECT *
+FROM employees
+
+-- get current manager name of Sales department (use subqueries)
+-- get all senior engineers in customer service department
+-- get all first and last names of current Sales department employees
+-- get the name of the employee with the highest salary
